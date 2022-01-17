6110646488a483b9159225a8544e9e502179c93b
beomjo <qjatjr1108@gmail.com>
2022-01-17 17:20:52 +0900

    Add dependency resolution managing setting to setting.gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,14 @@
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 enableFeaturePreview("VERSION_CATALOGS")
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("deps") {
+            from(files("deps.version.toml"))
+        }
+    }
+}
+
 rootProject.name = "MovieDatabase"
 include(":app")
 include(":domain")
