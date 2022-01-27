23f4d4f49c116a40d5214c79983eaeb6f0067235
beomjo <qjatjr1108@gmail.com>
2022-01-27 23:52:47 +0900

    feat: Add Singletone module

---
+++ app/src/main/java/io/beomjo/moviedb/di/SingletonModule.kt
@@ -0,0 +1,20 @@
+package io.beomjo.moviedb.di
+
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.beomjo.moviedb.BuildConfig
+import io.beomjo.moviedb.domain.ApiKey
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal object SingletonModule {
+
+    @Provides
+    @ApiKey
+    fun provideApiKey(): String {
+        return BuildConfig.REST_KEY
+    }
+}
---
+++ domain/src/main/java/io/beomjo/moviedb/domain/ApiKey.kt
@@ -0,0 +1,7 @@
+package io.beomjo.moviedb.domain
+
+import javax.inject.Qualifier
+
+@Retention(AnnotationRetention.RUNTIME)
+@Qualifier
+annotation class ApiKey
