83bb9319b83575a8b4396d5f40154361e86953f7
beomjo <qjatjr1108@gmail.com>
2022-02-07 03:15:51 +0900

    feat: Add Navigation extention

--- navigation/src/main/java/io/beomjo/moviedb/navigation/Route.kt
+++ navigation/src/main/java/io/beomjo/moviedb/navigation/Route.kt
@@ -1,9 +1,40 @@
-
 package io.beomjo.moviedb.navigation
 
-sealed class Route(val name: String) {
-    object Home : Route("home")
-    object Search : Route("search")
-    object People : Route("people")
-    object MyPage : Route("mypage")
+enum class Route(val path: String) {
+    Home("home") {
+        override val isTabRoute: Boolean
+            get() = true
+
+        override val children: List<Route>
+            get() = super.children
+    },
+
+    Search("search") {
+        override val isTabRoute: Boolean
+            get() = true
+        override val children: List<Route>
+            get() = super.children
+    },
+
+    People("people") {
+        override val isTabRoute: Boolean
+            get() = true
+        override val children: List<Route>
+            get() = super.children
+    },
+
+    MyPage("mypage") {
+        override val isTabRoute: Boolean
+            get() = true
+        override val children: List<Route>
+            get() = super.children
+    };
+
+    open val isTabRoute = false
+
+    open val children: List<Route> = listOf()
+
+    companion object {
+        val default = Home
+    }
 }
