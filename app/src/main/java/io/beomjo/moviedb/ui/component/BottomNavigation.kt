7a43d0b94821f3cefd16865759eceac30be27ccc
beomjo <qjatjr1108@gmail.com>
2022-02-12 01:35:50 +0900

    feat: Add bottomTab icon

--- app/src/main/java/io/beomjo/moviedb/ui/component/BottomNavigation.kt
+++ app/src/main/java/io/beomjo/moviedb/ui/component/BottomNavigation.kt
@@ -1,11 +1,13 @@
 package io.beomjo.moviedb.ui.component
 
+import androidx.compose.foundation.Image
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -22,24 +24,24 @@ fun BottomNavigation(
     val tabs = Tab.values().toList()
     BottomNavigation {
         tabs.forEach { tab ->
+            val isSelected = currentRoute.contains(tab.path)
             BottomNavigationItem(
                 icon = {
-//                    Image(R
-//                        painterResource(tab.icon),
-//                        contentDescription = "",
-//                    )
+                    Image(
+                        painterResource(tab.iconRes),
+                        alpha = if (isSelected) 1f else 0.5f,
+                        contentDescription = "",
+                    )
                 },
                 label = { Text(text = tab.name, fontSize = 9.sp) },
                 selectedContentColor = Color.White,
                 unselectedContentColor = Color.White.copy(0.4f),
                 alwaysShowLabel = true,
-                selected = currentRoute.contains(tab.path),
+                selected = isSelected,
                 onClick = {
                     navController.navigate(tab.path) {
                         launchSingleTop = true
                         restoreState = true
-                        // Pop up backstack to the first destination and save state. This makes going back
-                        // to the start destination when pressing back in any other bottom tab.
                         val startDestination = findStartDestination(navController.graph)
                         popUpTo(startDestination.id) {
                             saveState = true
--- navigation/src/main/java/io/beomjo/moviedb/navigation/Tab.kt
+++ navigation/src/main/java/io/beomjo/moviedb/navigation/Tab.kt
@@ -1,8 +1,13 @@
 package io.beomjo.moviedb.navigation
 
-enum class Tab(val path: String) {
-    Home("home"),
-    Search("search"),
-    People("people"),
-    MyPage("mypage");
+import androidx.annotation.DrawableRes
+
+enum class Tab(
+    val path: String,
+    @DrawableRes val iconRes: Int,
+) {
+    Home("home", R.drawable.ic_home),
+    Search("search", R.drawable.ic_search),
+    People("people", R.drawable.ic_people),
+    MyPage("mypage", R.drawable.ic_more);
 }
